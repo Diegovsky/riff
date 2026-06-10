0eb0ce6ae126bd8d68183213f3e20e9d05e69f89
Joshua de Jong <joshuakdejong@gmail.com>
2026-06-09 21:24:53 -0700

    Organized components into shell/ widgets/ and pages/

---
+++ src/app/components/widgets/mod.rs
@@ -0,0 +1,14 @@
+pub mod card;
+pub use card::*;
+
+pub mod card_list;
+pub use card_list::*;
+
+pub mod details_page;
+pub use details_page::*;
+
+pub mod playlist;
+pub use playlist::*;
+
+pub mod selection;
+pub use selection::*;
